6aa03e9b606cb82d5456ab6008e5de882db971c4
Alexei <lexx.kg@gmail.com>
2019-11-19 21:05:24 +0600

    Basic crud operations 2

--- myapp/mypages/urls.py
+++ myapp/mypages/urls.py
@@ -2,7 +2,18 @@ from django.urls import path
 
 from . import views as v
 
+# products/create
+# products/list
+# products/view/5
+# products/edit/5
+# products/delete/5
+
+# CRUD - Create Read Update Delete
+
 urlpatterns = [
     path('', v.startpage, name='index'),
+    path('products', v.product_handler, name='products'),
+    path('products/<str:action>', v.product_handler, name='products'),
+    path('products/<str:action>/<int:product_id>', v.product_handler, name='products'),
     path('test', v.starttest, name='test')
 ]
--- myapp/mypages/views.py
+++ myapp/mypages/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import redirect
 from .models import Product
 
 # Create your views here.
@@ -26,3 +27,47 @@ def startpage(request):
 
 def starttest(request):
     return HttpResponse('Test response')
+
+def product_handler(request, action = None, product_id = None):
+    context = {}
+    if action == 'list':
+        context['products'] = Product.objects.all()
+        template_name = 'products_list.html'
+    elif action == 'create':
+        new_data = request.GET
+        if new_data:
+            new_product = Product(
+                name = new_data['product_name'],
+                discount = False,
+                price = new_data['product_price'],
+                description = new_data['product_description']
+            )
+            new_product.save()
+            return redirect('/products/list')
+        context['is_edit'] = False
+        template_name = 'products_form.html'
+    elif action == 'view' and product_id:
+        context['product'] = Product.objects.get(id=product_id)
+        template_name = 'products_view.html'
+    elif action == 'edit' and product_id:
+        prod_data = request.GET
+        product = Product.objects.get(id=product_id)
+        if prod_data:
+            product.name = prod_data['product_name']
+            product.price = prod_data['product_price']
+            product.description = prod_data['product_description']
+            product.save()
+        context['is_edit'] = True
+        context['product'] = product
+        template_name = 'products_form.html'
+    elif action == 'delete' and product_id:
+        product = Product.objects.get(id=product_id)
+        product.delete()
+        response = redirect('/products/list')
+        return response
+    else:
+        return redirect('/products/list')
+
+    return render(request, template_name, context)
+
+    return HttpResponse('Test response' + action + ' ' + str(product_id))
